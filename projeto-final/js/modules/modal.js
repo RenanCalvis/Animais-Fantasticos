2242e1660d65f86ddd90f6aeb8a76f346bff7081
RenanCalvis <renancalvis@gmail.com>
2024-12-18 15:17:22 -0400

    Class Modal

--- projeto-final/js/modules/modal.js
+++ projeto-final/js/modules/modal.js
@@ -1,20 +1,38 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class Modal {
+  constructor(botaoAbrir, botaoFechar, containerModal) {
+    this.botaoAbrir = document.querySelector(botaoAbrir);
+    this.botaoFechar = document.querySelector(botaoFechar);
+    this.containerModal = document.querySelector(containerModal);
+    this.containerAtivo = "ativo";
 
-  function toggleModal(event) {
-    event.preventDefault();
-    containerModal.classList.toggle("ativo");
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.fecharForaMoral = this.fecharForaMoral.bind(this);
+  }
+
+  toggleModal() {
+    this.containerModal.classList.toggle(this.containerAtivo);
     // console.log(event);
   }
-  function fecharForaMoral(event) {
-    if (this === event.target) toggleModal(event);
+
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
   }
 
-  if (botaoAbrir && botaoFechar && containerModal) {
-    botaoAbrir.addEventListener("click", toggleModal);
-    botaoFechar.addEventListener("click", toggleModal);
-    containerModal.addEventListener("click", fecharForaMoral);
+  fecharForaMoral(event) {
+    if (event.target === this.containerModal) this.toggleModal(event);
+  }
+
+  addModalEvents() {
+    this.botaoAbrir.addEventListener("click", this.eventToggleModal);
+    this.botaoFechar.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.fecharForaMoral);
+  }
+
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addModalEvents();
+    }
+    return this;
   }
 }
