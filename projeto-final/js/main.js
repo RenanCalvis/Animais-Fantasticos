1e185b719bc05086c0a34cddf3b40b70285524af
RenanCalvis <renancalvis@gmail.com>
2024-12-19 17:50:28 -0400

    refatoração  fetchAnimais

--- projeto-final/js/main.js
+++ projeto-final/js/main.js
@@ -5,7 +5,7 @@ import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
+import fetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
@@ -36,5 +36,6 @@ initAnimacaoScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
+
+fetchAnimais("animaisApi.json", ".numeros-grid");
