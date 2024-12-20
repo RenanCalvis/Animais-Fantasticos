fd7ae76f58732021aa0adf0ff75ec445cc44624c
RenanCalvis <renancalvis@gmail.com>
2024-12-20 16:37:15 -0400

    class para ScrollAnima

--- projeto-final/js/main.js
+++ projeto-final/js/main.js
@@ -1,4 +1,4 @@
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import ScrollAnima from "./modules/scroll-anima.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
@@ -32,7 +32,9 @@ modal.init();
 const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
 
-initAnimacaoScroll();
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
---
+++ projeto-final/js/modules/scroll-anima.js
@@ -0,0 +1,24 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+    this.activeClass = "ativo";
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowMetade < 0;
+      if (isSectionVisible) section.classList.add(this.activeClass);
+      else if (section.classList.contains(this.activeClass)) {
+        section.classList.remove(this.activeClass);
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener("scroll", this.animaScroll);
+  }
+}
