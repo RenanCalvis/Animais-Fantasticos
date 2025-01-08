7549f989cc0f2137a582cf544715e6e40c2e2e01
RenanCalvis <renancalvis@gmail.com>
2025-01-08 16:11:14 -0400

    refatoração menu mobile

--- projeto-final/js/main.js
+++ projeto-final/js/main.js
@@ -2,13 +2,13 @@ import ScrollAnima from "./modules/scroll-anima.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
-import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
+import MenuMobile from "./modules/menu-mobile.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -37,7 +37,10 @@ scrollAnima.init();
 
 const dropdownMenus = new DropdownMenu("[data-dropdown]");
 dropdownMenus.init();
-initMenuMobile();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 initFuncionamento();
 
 fetchAnimais("animaisApi.json", ".numeros-grid");
